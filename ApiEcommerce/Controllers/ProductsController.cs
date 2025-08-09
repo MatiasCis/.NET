74031d329681a490b037e0547087c452f7b047c4
MatiasCis <mgcm03@hotmail.com>
2025-08-09 00:14:03 -0400

    07.5

---
+++ ApiEcommerce/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+using ApiEcommerce.Models.Dtos;
+using ApiEcommerce.Repository.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public IActionResult GetProducts()
+        {
+            var products = _productRepository.GetProducts();
+            var productsDto = _mapper.Map<List<ProductDto>>(products);
+
+            return Ok(productsDto);
+        }
+
+        [HttpGet("{productId:int}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetProduct(int productId)
+        {
+            var products = _productRepository.GetProducts(productId);
+            if (products == null)
+            {
+                return NotFound($"La categoria con el {productId} no existe");
+            }
+            var productsDto = _mapper.Map<ProductDto>(products);
+
+            return Ok(productsDto);
+        }
+    }
+}
--- ApiEcommerce/Data/ApplicationDbContext.cs
+++ ApiEcommerce/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using ApiEcommerce.Models;
 using Microsoft.EntityFrameworkCore;
 public class ApplicationDbContext : DbContext
 {
@@ -6,4 +7,5 @@ public class ApplicationDbContext : DbContext
 
     }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
 }
