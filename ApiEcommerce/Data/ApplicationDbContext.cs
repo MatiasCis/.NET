6f88d429be8850d7547385d503cf37ff33a1108b
MatiasCis <mgcm03@hotmail.com>
2025-08-10 01:37:53 -0400

    08.5

--- ApiEcommerce/Data/ApplicationDbContext.cs
+++ ApiEcommerce/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
 using ApiEcommerce.Models;
 using Microsoft.EntityFrameworkCore;
+
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
 
-    }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
 }
